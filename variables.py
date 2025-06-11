3c87cbc55c97e9744a6f5e3fe62324c8ff94d090
Micah Tobias <naturalmikky@yahoo.com>
2025-06-12 01:27:02 +0300

    commiting variable.py

---
+++ variables.py
@@ -0,0 +1,11 @@
+# defining variables
+
+launches_num = 4
+rocket_name = "Mikky fly"
+success_rate = 0.9
+
+
+# print out the variable
+
+print(f"final_state = {rocket_name} has flew {launches_num} times with {success_rate*100}%")
+
